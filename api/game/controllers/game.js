214ca05ba06adaba741bfc5c9e6c8890f44defaa
Vinicius <beneditoloura@gmail.com>
2021-08-31 08:36:56 -0300

    feat: create first strapi service

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,13 +1,11 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
 module.exports = {
-  populate: (ctx) => {
-    console.log("initialize")
-    ctx.send({ ok: true })
+  populate: async (ctx) => {
+    console.log("Starting to populate...")
+
+    await strapi.services.game.populate()
+
+    ctx.send("Finished populating!")
   }
 };
