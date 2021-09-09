b6878c5876da98695c16d7b21ca9b7a8dd1ae3d2
Vinicius <beneditoloura@gmail.com>
2021-09-09 08:08:36 -0300

    chore: install jsdom

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,11 +7,26 @@
 
 const axios = require('axios');
 
+async function getGameInfo(slug) {
+  const jsdom = require('jsdom');
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector('.description');
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
 
     const { data: { products } } = await axios.get(gogApiUrl)
-    console.log(products[0])
+    console.log(await getGameInfo(products[0].slug))
   }
 };
