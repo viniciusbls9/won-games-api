56bbf8c738e68a53a74555568e23397313466192
Vinicius <beneditoloura@gmail.com>
2021-09-14 09:05:24 -0300

    refactor: change api service name

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -39,16 +39,49 @@ async function create(name, entityName) {
   }
 }
 
+async function createManyToManyData(products) {
+  const developers = new Set();
+  const publishers = new Set();
+  const categories = new Set();
+  const platforms = new Set();
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres?.forEach((item) => {
+      categories.add(item);
+    });
+
+    supportedOperatingSystems?.forEach((item) => {
+      platforms.add(item);
+    });
+
+    developers.add(developer);
+    publishers.add(publisher);
+  });
+
+  const createCall = (set, entityName) => Array.from(set).map((name) => create(name, entityName));
+
+  return Promise.all([
+    ...createCall(developers, "developer"),
+    ...createCall(publishers, "publisher"),
+    ...createCall(categories, "category"),
+    ...createCall(platforms, "platform"),
+  ]);
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
 
     const { data: { products } } = await axios.get(gogApiUrl)
 
-    console.log(products[0])
+    // console.log(products[0])
 
-    await create(products[1].publisher, "publisher")
-    await create(products[1].developer, "developer")
+    await createManyToManyData([products[3], products[4]])
+
+    // await create(products[1].publisher, "publisher")
+    // await create(products[1].developer, "developer")
 
     // console.log(await getGameInfo(products[0].slug))
   }
