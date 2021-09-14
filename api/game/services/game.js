d30be18c091ac6efc9e7b641215f74a7fd49b311
Vinicius <beneditoloura@gmail.com>
2021-09-14 08:27:51 -0300

    feat: create function to create new publisher and developer

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -23,6 +23,22 @@ async function getGameInfo(slug) {
   }
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name })
+  return item.length ? item[0] : null
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName)
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true })
+    })
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
@@ -31,7 +47,9 @@ module.exports = {
 
     console.log(products[0])
 
-    await strapi.services.developer.create({ name: products[0].publisher, slug: slugify(products[0].publisher).toLowerCase() })
+    await create(products[1].publisher, "publisher")
+    await create(products[1].developer, "developer")
+
     // console.log(await getGameInfo(products[0].slug))
   }
 };
