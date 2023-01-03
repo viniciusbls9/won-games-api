c8c8563b9891e57ff0a8b38cd5adcd7d17cf9487
Vinicius <beneditoloura@gmail.com>
2023-01-03 09:12:58 -0300

    refactor: change database data

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'wongames'),
-        username: env('DATABASE_USERNAME', 'wongames'),
-        password: env('DATABASE_PASSWORD', 'wongames123'),
+        database: env('DATABASE_NAME', 'strapidb'),
+        username: env('DATABASE_USERNAME', 'strapiuser'),
+        password: env('DATABASE_PASSWORD', 'strapiPass'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
