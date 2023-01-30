9bc0a509df864a1f970210563c1ab7e1f8c194f1
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-30 08:25:48 -0300

    update db data

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapidb'),
-        username: env('DATABASE_USERNAME', 'strapiuser'),
-        password: env('DATABASE_PASSWORD', 'strapiPass'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
